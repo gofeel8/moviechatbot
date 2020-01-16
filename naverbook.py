d09d84cf187f6ca48fe03e98ebce7c430a297592
gofeel8 <34854154+gofeel8@users.noreply.github.com>
2020-01-16 17:28:25 +0900

    final

--- naverbook.py
+++ naverbook.py
@@ -11,16 +11,16 @@ def process_webhook():
 
     print(request_json["queryResult"]["parameters"])
 
-    # naver-book 인텐트 중 book 파라미터 데이터 추출
+    # 인증 정보
+    client_id = "jNok5qfqfnVlZR_wV1ns"
+    client_secret = "IiwFA0bYPc"
+    # 기본 url 정보
+    url = "https://openapi.naver.com/v1/search/movie.json"
+
     if "movie" in request_json["queryResult"]["parameters"]:
         query = request_json["queryResult"]["parameters"]["movie"]
         print(query)
-        # 인증 정보
-        client_id = "jNok5qfqfnVlZR_wV1ns"
-        client_secret = "IiwFA0bYPc"
 
-        # 기본 url 정보
-        url = "https://openapi.naver.com/v1/search/movie.json"
 
         # url 호출 시 전달할 요청 변수 정보
         params = {"query": query,
@@ -34,29 +34,13 @@ def process_webhook():
                                          "Content-Type": "application/json; charset=utf-8"})
         # 호출 처리 상태 정보 recode 변수에 할당
         rescode = response.status_code
-
         if (rescode == 200):
-            # 호출 처리 상태가 정상(200) 일 경우리턴 받은 책 조회 정보 출력
-            # pprint.pprint(response.json())
             data = response.json()
         else:
             print("Error Code:", rescode)
 
-        # print(data)
-        # Naver 책 검색 API 응답 중 실제 책 아이템 데이터 추출 및 출력
         item_list = data["items"]
-        # pprint.pprint(item_list)
-        # print(item_list[0]['actor'])
 
-        # print(answer)
-        # Dialogflow로 응답되는 최종 문자열 데이터 구성
-        # book_list = ""
-        # for item in item_list:
-        #     book_list += item["title"]
-        #     book_list += " "
-
-        # Dialogflow로 응답되는 최종 데이터 확인
-        # print(book_list)
         title = BeautifulSoup(item_list[0]['title'],'html.parser').text
         print(len(item_list[0]['actor']))
         if len(item_list[0]['actor']) >50:
@@ -80,6 +64,8 @@ def process_webhook():
             }
           ]}
 
+
+
     if request_json["queryResult"]["parameters"]["rank"]:
         user_agent = UserAgent()
         main_url = "http://www.cgv.co.kr/movies/"
@@ -91,13 +77,11 @@ def process_webhook():
         rank_list = []
 
         for gps in rank:
-            # a=gps.strong
             title = BeautifulSoup(gps.strong.text, 'html.parser').text
             rank_list.append(title)
-            # print(title)  # 제목
 
-        print(rank_list)
-        return {"fulfillmentText": "1위: "+rank_list[0]+"\n"+"2위: "+rank_list[1]+"\n"+"3위: "+rank_list[2]+"\n"+"4위: "+rank_list[3]+"\n"+"5위: "+rank_list[4]+"\n"+"6위: "+rank_list[5]+"\n"+"7위: "+rank_list[6]}
+    return {"fulfillmentText": "1위: "+rank_list[0]+"\n"+"2위: "+rank_list[1]+"\n"+"3위: "+rank_list[2]+"\n"+"4위: "+rank_list[3]+"\n"+"5위: "+rank_list[4]+"\n"+"6위: "+rank_list[5]+"\n"+"7위: "+rank_list[6]}
+
 
 
 if __name__ == '__main__':
